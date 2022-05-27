d3c72d2e9eb6708d9df7279c06e43041b6673127
StefPetro <43845091+StefPetro@users.noreply.github.com>
2022-05-27 17:00:10 +0200

    Changed to tailwind  v3 and changed content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./index.html"
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
